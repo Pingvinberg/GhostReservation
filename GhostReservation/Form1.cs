02c91b22e734534c843aa104c9e0e1436ffe2484
Pingvinberg <Pingvinberg@gmail.com>
2022-04-26 20:24:53 +0200

    SQL command

--- GhostReservation/Form1.cs
+++ GhostReservation/Form1.cs
@@ -40,7 +40,18 @@ namespace GhostReservation
                 "Initial Catalog=DatabaseName;" +
                 "User ID=UserName;" +
                 "Password=Password";
-            sql = "Your SQL Statement Here , like Select * from product" + storeID + "blabla" + articleId;
+            sql = $"DECLARE @StoreNo as int" +
+               "DECLARE @Article as int" +
+               "DECLARE @Articleno as int" +
+               "DECLARE @Article as varchar (20)" +
+               "set @StoreNo = @StoreNo" +
+               "set @SupplierArticleId = @SupplierArticleId" +
+               "set @ArticleId = (Select ArticleID from AllArticles where SupplierArticleID = @SupplierArticleId)" +
+               "set @Articleno = (Select ArticleNo from AllArticles where ArticleId = @ArticleId)" +
+               "SELECT TOP 1000 CustomerOrderStatus, ArticleID, CustomerOrderLineStatus, ExternalOrderId, OrderedQty, ReceivedQty,  rex.* FROM customerorders co with(nolock) JOIN customerorderlines col with(nolock) ON co.CustomerOrderNo = col.CustomerOrderNo JOIN openquery ('MYSQL-PHARMASUITE', 'SELECT RB.bestallningsDatum,       RB.salesOrderId,ERPARTIKEL.varunummer, RBR.receptBestallningsRadStatus_id, RB.receptBestallningsStatus_id, RBR.ARTIKEL_ID FROM RECEPTBESTALLNING RB JOIN RECEPTBESTALLNINGRAD RBR ON RB.id = RBR.bestallning_id INNER JOIN ERPARTIKEL ON RBR.artikel_id = ERPARTIKEL.id where ERPARTIKEL.varunummer = @SupplierArticleId; ') rex ON rex.salesOrderId = co.CustomerOrderID AND rex.ARTIKEL_ID = col.ArticleID WHERE COL.ArticleID=@ArticleId AND co.StoreNo = @StoreNo and rex.ARTIKEL_ID = @ArticleId and co.CustomerOrderStatus <> 80 and CustomerOrderStatus <> 99 order by CO.CustomerOrderNo DESC" +
+               "select AllArticles.ArticleID, AllArticles.SupplierArticleID, StoreArticleInfos.ArticleNo, ReservedStockQty, TotalStockQty, InStockQty, StockInOrderQty, ReservedStockInOrderQty, AvailableStockQty from StoreArticleInfos with(nolock) inner join AllArticles with(nolock) on AllArticles.ArticleNo = StoreArticleInfos.ArticleNo where StoreNo = @Storeno and StoreArticleInfos.ArticleNo = @Articleno" +
+               "select top 5 * from StockAdjustments with(nolock) where StoreNo = @Storeno and ArticleNo = @Articleno order by AdjustmentDate desc";
+            //"Your SQL Statement Here , like Select * from product" + storeID + "blabla" + articleId;
             connection = new SqlConnection(connectionString);
             try
             {
