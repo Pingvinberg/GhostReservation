27ec8a51f6587a7c7c8ef2a7f16fb6fbd5cdca54
Pingvinberg <Pingvinberg@gmail.com>
2022-04-26 19:31:18 +0200

    Add project files.

---
+++ GhostReservation/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GhostReservation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }      
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.storeIdBox = new System.Windows.Forms.TextBox();
+            this.articleIDBox = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // storeIdBox
+            // 
+            this.storeIdBox.Location = new System.Drawing.Point(100, 64);
+            this.storeIdBox.Name = "storeIdBox";
+            this.storeIdBox.Size = new System.Drawing.Size(67, 23);
+            this.storeIdBox.TabIndex = 0;
+            // 
+            // articleIDBox
+            // 
+            this.articleIDBox.Location = new System.Drawing.Point(100, 93);
+            this.articleIDBox.Name = "articleIDBox";
+            this.articleIDBox.Size = new System.Drawing.Size(178, 23);
+            this.articleIDBox.TabIndex = 1;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(100, 122);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(503, 275);
+            this.resultBox.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(100, 35);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(508, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Copy (ctrl + c)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.articleIDBox);
+            this.Controls.Add(this.storeIdBox);
+            this.Name = "Form1";
+            this.Text = "Ghost Reservation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox storeIdBox;
+        private TextBox articleIDBox;
+        private TextBox resultBox;
+        private Button btnSearch;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ GhostReservation/Form1.cs
@@ -0,0 +1,68 @@
+using System.Data.SqlClient;
+
+namespace GhostReservation
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {   
+            resultBox.Text = sqlQuery(storeIdBox.Text, articleIDBox.Text);
+        }
+
+        private string sqlQuery(string storeID, string articleId)
+        {
+            string result = null;
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sql = null;
+            SqlDataReader dataReader;
+            connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password";
+            sql = "Your SQL Statement Here , like Select * from product" + storeID +"blabla" + articleId;
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    /*MessageBox.Show(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2));
+                     Hantera datan som kommer i dataReader.GetValeue(0).tostring för få första värdet och hantera det.
+                     dvs skapa en string här som vi skickar till resultbox.text
+                     */
+                }
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+            result = "Store ID: " + storeID + "\r\n" + "Article ID: " + articleId; //bara för att testa copy funktionen
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = "";
+            storeIdBox.Text = "";
+            articleIDBox.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resultBox.SelectAll();
+            resultBox.Copy();
+            resultBox.DeselectAll();
+        }
+    }
+}
