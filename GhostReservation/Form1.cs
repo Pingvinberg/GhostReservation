81b2e1ac9c2acfb531539df09d60513ed2642502
Pingvinberg <Pingvinberg@gmail.com>
2022-04-26 20:27:15 +0200

    update sql command

--- GhostReservation/Form1.cs
+++ GhostReservation/Form1.cs
@@ -51,7 +51,6 @@ namespace GhostReservation
                "SELECT TOP 1000 CustomerOrderStatus, ArticleID, CustomerOrderLineStatus, ExternalOrderId, OrderedQty, ReceivedQty,  rex.* FROM customerorders co with(nolock) JOIN customerorderlines col with(nolock) ON co.CustomerOrderNo = col.CustomerOrderNo JOIN openquery ('MYSQL-PHARMASUITE', 'SELECT RB.bestallningsDatum,       RB.salesOrderId,ERPARTIKEL.varunummer, RBR.receptBestallningsRadStatus_id, RB.receptBestallningsStatus_id, RBR.ARTIKEL_ID FROM RECEPTBESTALLNING RB JOIN RECEPTBESTALLNINGRAD RBR ON RB.id = RBR.bestallning_id INNER JOIN ERPARTIKEL ON RBR.artikel_id = ERPARTIKEL.id where ERPARTIKEL.varunummer = @SupplierArticleId; ') rex ON rex.salesOrderId = co.CustomerOrderID AND rex.ARTIKEL_ID = col.ArticleID WHERE COL.ArticleID=@ArticleId AND co.StoreNo = @StoreNo and rex.ARTIKEL_ID = @ArticleId and co.CustomerOrderStatus <> 80 and CustomerOrderStatus <> 99 order by CO.CustomerOrderNo DESC" +
                "select AllArticles.ArticleID, AllArticles.SupplierArticleID, StoreArticleInfos.ArticleNo, ReservedStockQty, TotalStockQty, InStockQty, StockInOrderQty, ReservedStockInOrderQty, AvailableStockQty from StoreArticleInfos with(nolock) inner join AllArticles with(nolock) on AllArticles.ArticleNo = StoreArticleInfos.ArticleNo where StoreNo = @Storeno and StoreArticleInfos.ArticleNo = @Articleno" +
                "select top 5 * from StockAdjustments with(nolock) where StoreNo = @Storeno and ArticleNo = @Articleno order by AdjustmentDate desc";
-            //"Your SQL Statement Here , like Select * from product" + storeID + "blabla" + articleId;
             connection = new SqlConnection(connectionString);
             try
             {
@@ -60,18 +59,7 @@ namespace GhostReservation
                 dataReader = command.ExecuteReader();
                 while (dataReader.Read())
                 {
-                    /*MessageBox.Show(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2));
-                     Hantera datan som kommer i dataReader.GetValeue(0).tostring för få första värdet och hantera det.
-                     dvs skapa en string här som vi skickar till resultbox.text
-
-                    ex.
-                    var data1 = dataReader.GetValue(0);
-                    var data2 = dataReader.GetValue(1);
-                    if (data1 > 0)
-                        {
-                            result = data1.toString() + " är större än 0" 
-                        }   
-                     */
+                    
                 }
                 dataReader.Close();
                 command.Dispose();
