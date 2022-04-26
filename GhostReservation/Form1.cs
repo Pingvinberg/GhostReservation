31004b2d10c6f1452eca7335ee3d6aa7dc5f1c4a
Pingvinberg <Pingvinberg@gmail.com>
2022-04-26 21:38:39 +0200

    Update parameters for sql command

--- GhostReservation/Form1.cs
+++ GhostReservation/Form1.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -23,7 +24,7 @@ namespace GhostReservation
             {
                 resultBox.Text = sqlQuery(storeIdBox.Text, articleIDBox.Text);
             }
-            
+
         }
 
         private string sqlQuery(string storeID, string articleId)
@@ -35,7 +36,7 @@ namespace GhostReservation
             SqlCommand command;
             string sql = null;
             SqlDataReader dataReader;
-            connectionString = 
+            connectionString =
                 "Data Source=ServerName;" +
                 "Initial Catalog=DatabaseName;" +
                 "User ID=UserName;" +
@@ -44,11 +45,11 @@ namespace GhostReservation
                "DECLARE @Article as int" +
                "DECLARE @Articleno as int" +
                "DECLARE @Article as varchar (20)" +
-               "set @StoreNo = " + storeID +
-               "set @SupplierArticleId = " + articleId +
+               "set @StoreNo = @StoreNo" +
+               "set @SupplierArticleId = @SupplierArticleId" +
                "set @ArticleId = (Select ArticleID from AllArticles where SupplierArticleID = @SupplierArticleId)" +
                "set @Articleno = (Select ArticleNo from AllArticles where ArticleId = @ArticleId)" +
-               "SELECT TOP 1000 CustomerOrderStatus, ArticleID, CustomerOrderLineStatus, ExternalOrderId, OrderedQty, ReceivedQty,  rex.* FROM customerorders co with(nolock) JOIN customerorderlines col with(nolock) ON co.CustomerOrderNo = col.CustomerOrderNo JOIN openquery ('MYSQL-PHARMASUITE', 'SELECT RB.bestallningsDatum,       RB.salesOrderId,ERPARTIKEL.varunummer, RBR.receptBestallningsRadStatus_id, RB.receptBestallningsStatus_id, RBR.ARTIKEL_ID FROM RECEPTBESTALLNING RB JOIN RECEPTBESTALLNINGRAD RBR ON RB.id = RBR.bestallning_id INNER JOIN ERPARTIKEL ON RBR.artikel_id = ERPARTIKEL.id where ERPARTIKEL.varunummer = " + articleId +"; ') rex ON rex.salesOrderId = co.CustomerOrderID AND rex.ARTIKEL_ID = col.ArticleID WHERE COL.ArticleID=@ArticleId AND co.StoreNo = @StoreNo and rex.ARTIKEL_ID = @ArticleId and co.CustomerOrderStatus <> 80 and CustomerOrderStatus <> 99 order by CO.CustomerOrderNo DESC" +
+               "SELECT TOP 1000 CustomerOrderStatus, ArticleID, CustomerOrderLineStatus, ExternalOrderId, OrderedQty, ReceivedQty,  rex.* FROM customerorders co with(nolock) JOIN customerorderlines col with(nolock) ON co.CustomerOrderNo = col.CustomerOrderNo JOIN openquery ('MYSQL-PHARMASUITE', 'SELECT RB.bestallningsDatum,       RB.salesOrderId,ERPARTIKEL.varunummer, RBR.receptBestallningsRadStatus_id, RB.receptBestallningsStatus_id, RBR.ARTIKEL_ID FROM RECEPTBESTALLNING RB JOIN RECEPTBESTALLNINGRAD RBR ON RB.id = RBR.bestallning_id INNER JOIN ERPARTIKEL ON RBR.artikel_id = ERPARTIKEL.id where ERPARTIKEL.varunummer = " + articleId + "; ') rex ON rex.salesOrderId = co.CustomerOrderID AND rex.ARTIKEL_ID = col.ArticleID WHERE COL.ArticleID=@ArticleId AND co.StoreNo = @StoreNo and rex.ARTIKEL_ID = @ArticleId and co.CustomerOrderStatus <> 80 and CustomerOrderStatus <> 99 order by CO.CustomerOrderNo DESC" +
                "select AllArticles.ArticleID, AllArticles.SupplierArticleID, StoreArticleInfos.ArticleNo, ReservedStockQty, TotalStockQty, InStockQty, StockInOrderQty, ReservedStockInOrderQty, AvailableStockQty from StoreArticleInfos with(nolock) inner join AllArticles with(nolock) on AllArticles.ArticleNo = StoreArticleInfos.ArticleNo where StoreNo = @Storeno and StoreArticleInfos.ArticleNo = @Articleno" +
                "select top 5 * from StockAdjustments with(nolock) where StoreNo = @Storeno and ArticleNo = @Articleno order by AdjustmentDate desc";
             connection = new SqlConnection(connectionString);
@@ -56,6 +57,9 @@ namespace GhostReservation
             {
                 connection.Open();
                 command = new SqlCommand(sql, connection);
+                command.CommandType = CommandType.Text;
+                command.Parameters.AddWithValue("StoreNo", storeID);
+                command.Parameters.AddWithValue("SupplierArticleId", articleId);
                 dataReader = command.ExecuteReader();
                 while (dataReader.Read())
                 {
@@ -82,12 +86,12 @@ namespace GhostReservation
                         "Source: " + ex.Errors[i].Source + "\n" +
                         "Procedure: " + ex.Errors[i].Procedure + "\n");
                 }
-                result =errorMessages.ToString();
+                result = errorMessages.ToString();
             }
             catch (Exception ex)
             {
                 result = ex.ToString();
-            }            
+            }
             return result;
         }
 
@@ -96,7 +100,7 @@ namespace GhostReservation
             if (storeIdBox.Text.Trim() == String.Empty)
                 return true;
             if (articleIDBox.Text.Trim() == String.Empty)
-                return true;            
+                return true;
             return false;
         }
 
