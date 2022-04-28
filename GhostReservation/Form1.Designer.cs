c9c9cd5e4034e9211c607596805b1078ddd55a53
Pingvinberg <Pingvinberg@gmail.com>
2022-04-29 00:30:28 +0200

    Uppdaterade SQL för beställnings id.

--- GhostReservation/Form1.Designer.cs
+++ GhostReservation/Form1.Designer.cs
@@ -34,33 +34,38 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.SupplierArticleIDBox = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // storeIdBox
             // 
-            this.storeIdBox.Location = new System.Drawing.Point(100, 64);
+            this.storeIdBox.Location = new System.Drawing.Point(195, 93);
             this.storeIdBox.Name = "storeIdBox";
             this.storeIdBox.Size = new System.Drawing.Size(67, 23);
             this.storeIdBox.TabIndex = 0;
             // 
             // articleIDBox
             // 
-            this.articleIDBox.Location = new System.Drawing.Point(100, 93);
+            this.articleIDBox.Location = new System.Drawing.Point(195, 153);
             this.articleIDBox.Name = "articleIDBox";
             this.articleIDBox.Size = new System.Drawing.Size(178, 23);
             this.articleIDBox.TabIndex = 1;
             // 
             // resultBox
             // 
-            this.resultBox.Location = new System.Drawing.Point(100, 122);
+            this.resultBox.Location = new System.Drawing.Point(100, 254);
             this.resultBox.Multiline = true;
             this.resultBox.Name = "resultBox";
-            this.resultBox.Size = new System.Drawing.Size(503, 275);
+            this.resultBox.Size = new System.Drawing.Size(503, 348);
             this.resultBox.TabIndex = 2;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(100, 35);
+            this.btnSearch.Location = new System.Drawing.Point(195, 59);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(75, 23);
             this.btnSearch.TabIndex = 3;
@@ -70,7 +75,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(212, 35);
+            this.button1.Location = new System.Drawing.Point(329, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(44, 23);
             this.button1.TabIndex = 4;
@@ -80,7 +85,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(508, 92);
+            this.button2.Location = new System.Drawing.Point(508, 225);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 23);
             this.button2.TabIndex = 5;
@@ -88,11 +93,49 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // SupplierArticleIDBox
+            // 
+            this.SupplierArticleIDBox.Location = new System.Drawing.Point(195, 124);
+            this.SupplierArticleIDBox.Name = "SupplierArticleIDBox";
+            this.SupplierArticleIDBox.Size = new System.Drawing.Size(178, 23);
+            this.SupplierArticleIDBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Store ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Supplier Article ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Article ID";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(808, 690);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SupplierArticleIDBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSearch);
@@ -101,6 +144,7 @@
             this.Controls.Add(this.storeIdBox);
             this.Name = "Form1";
             this.Text = "Ghost Reservation";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,5 +158,10 @@
         private Button btnSearch;
         private Button button1;
         private Button button2;
+        private TextBox SupplierArticleIDBox;
+        private FontDialog fontDialog1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- GhostReservation/Form1.cs
+++ GhostReservation/Form1.cs
@@ -13,43 +13,91 @@ namespace GhostReservation
             InitializeComponent();
         }
 
-
         private void btnSearch_Click_1(object sender, EventArgs e)
         {
-            if (WithErrors())
+            if (SupplierArticleIDBox.Text != String.Empty && articleIDBox.Text == String.Empty)
+            {
+                articleIDBox.Text = sqlQueryArticleID(SupplierArticleIDBox.Text);
+            }
+            else if (WithErrors())
             {
                 resultBox.Text = "One or several fields are empty";
             }
             else
             {
-                resultBox.Text = sqlQuery(storeIdBox.Text, articleIDBox.Text);
+                resultBox.Text = sqlQueryMain(storeIdBox.Text, articleIDBox.Text);
             }
-
         }
 
-        private string sqlQuery(string storeID, string articleId)
+        private string sqlQueryArticleID(string supplierArticleID)
         {
-
             string result = null;
             string connectionString = null;
+            string sql = null;
             SqlConnection connection;
             SqlCommand command;
-            string sql = null;
             SqlDataReader dataReader;
             connectionString =
                 "Data Source=ServerName;" +
                 "Initial Catalog=DatabaseName;" +
                 "User ID=UserName;" +
                 "Password=Password";
-            sql = $"DECLARE @StoreNo as int" +
-               "DECLARE @Article as int" +
-               "DECLARE @Articleno as int" +
-               "DECLARE @Article as varchar (20)" +
-               "set @StoreNo = @StoreID" +
-               "set @SupplierArticleId = @SArticleId" +
-               "set @ArticleId = (Select ArticleID from AllArticles where SupplierArticleID = @SupplierArticleId)" +
-               "set @Articleno = (Select ArticleNo from AllArticles where ArticleId = @ArticleId)" +
-               "SELECT TOP 1000 CustomerOrderStatus, ArticleID, CustomerOrderLineStatus, ExternalOrderId, OrderedQty, ReceivedQty,  rex.* FROM customerorders co with(nolock) JOIN customerorderlines col with(nolock) ON co.CustomerOrderNo = col.CustomerOrderNo JOIN openquery ('MYSQL-PHARMASUITE', 'SELECT RB.bestallningsDatum,       RB.salesOrderId,ERPARTIKEL.varunummer, RBR.receptBestallningsRadStatus_id, RB.receptBestallningsStatus_id, RBR.ARTIKEL_ID FROM RECEPTBESTALLNING RB JOIN RECEPTBESTALLNINGRAD RBR ON RB.id = RBR.bestallning_id INNER JOIN ERPARTIKEL ON RBR.artikel_id = ERPARTIKEL.id where ERPARTIKEL.varunummer = " + articleId + "; ') rex ON rex.salesOrderId = co.CustomerOrderID AND rex.ARTIKEL_ID = col.ArticleID WHERE COL.ArticleID=@ArticleId AND co.StoreNo = @StoreNo and rex.ARTIKEL_ID = @ArticleId and co.CustomerOrderStatus <> 80 and CustomerOrderStatus <> 99 order by CO.CustomerOrderNo DESC" +
+            sql = ""; // sql select för att få fram ArtikelId från @SupplierArticleID
+
+            connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                command.CommandType = CommandType.Text;
+                command.Parameters.AddWithValue("@SupplierArticleID", supplierArticleID);
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    result = dataReader["ArticleId"].ToString();
+                }
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorMessages.Append("Index #" + i + "\n" +
+                        "Message: " + ex.Errors[i].Message + "\n" +
+                        "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
+                        "Source: " + ex.Errors[i].Source + "\n" +
+                        "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+                resultBox.Text = errorMessages.ToString();
+            }
+            catch (Exception ex)
+            {
+                resultBox.Text = ex.ToString();
+            }
+            return result;
+        }
+
+        private string sqlQueryMain(string storeID, string articleId)
+        {
+            string result = null;
+            string connectionString = null;
+            string sql = null;
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataReader dataReader;
+            connectionString =
+                "Data Source=ServerName;" +
+                "Initial Catalog=DatabaseName;" +
+                "User ID=UserName;" +
+                "Password=Password";
+            sql = 
+               //"set @StoreNo = @StoreNo" +
+               //"set @SupplierArticleId = @SupplierArticleId" +
+               //"set @ArticleId = (Select ArticleID from AllArticles where SupplierArticleID = @SupplierArticleId)" +
+               //"set @Articleno = (Select ArticleNo from AllArticles where ArticleId = @ArticleId)" +
+               "SELECT TOP 1000 CustomerOrderStatus, ArticleID, CustomerOrderLineStatus, ExternalOrderId, OrderedQty, ReceivedQty,  rex.* FROM customerorders co with(nolock) JOIN customerorderlines col with(nolock) ON co.CustomerOrderNo = col.CustomerOrderNo JOIN openquery ('MYSQL-PHARMASUITE', 'SELECT RB.bestallningsDatum,       RB.salesOrderId,ERPARTIKEL.varunummer, RBR.receptBestallningsRadStatus_id, RB.receptBestallningsStatus_id, RBR.ARTIKEL_ID FROM RECEPTBESTALLNING RB JOIN RECEPTBESTALLNINGRAD RBR ON RB.id = RBR.bestallning_id INNER JOIN ERPARTIKEL ON RBR.artikel_id = ERPARTIKEL.id where ERPARTIKEL.varunummer = @SupplierArticleId; ') rex ON rex.salesOrderId = co.CustomerOrderID AND rex.ARTIKEL_ID = col.ArticleID WHERE COL.ArticleID=@ArticleId AND co.StoreNo = @StoreNo and rex.ARTIKEL_ID = @ArticleId and co.CustomerOrderStatus <> 80 and CustomerOrderStatus <> 99 order by CO.CustomerOrderNo DESC" +
                "select AllArticles.ArticleID, AllArticles.SupplierArticleID, StoreArticleInfos.ArticleNo, ReservedStockQty, TotalStockQty, InStockQty, StockInOrderQty, ReservedStockInOrderQty, AvailableStockQty from StoreArticleInfos with(nolock) inner join AllArticles with(nolock) on AllArticles.ArticleNo = StoreArticleInfos.ArticleNo where StoreNo = @Storeno and StoreArticleInfos.ArticleNo = @Articleno" +
                "select top 5 * from StockAdjustments with(nolock) where StoreNo = @Storeno and ArticleNo = @Articleno order by AdjustmentDate desc";
             connection = new SqlConnection(connectionString);
@@ -58,8 +106,8 @@ namespace GhostReservation
                 connection.Open();
                 command = new SqlCommand(sql, connection);
                 command.CommandType = CommandType.Text;
-                command.Parameters.AddWithValue("@StoreID", storeID);
-                command.Parameters.AddWithValue("@SArticleId", articleId);
+                command.Parameters.AddWithValue("@StoreNo", storeID);
+                command.Parameters.AddWithValue("@ArticleNo", articleId);
                 dataReader = command.ExecuteReader();
                 while (dataReader.Read())
                 {
@@ -109,6 +157,7 @@ namespace GhostReservation
             resultBox.Text = "";
             storeIdBox.Text = "";
             articleIDBox.Text = "";
+            SupplierArticleIDBox.Text = "";
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -116,6 +165,11 @@ namespace GhostReservation
             resultBox.SelectAll();
             resultBox.Copy();
             resultBox.DeselectAll();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
