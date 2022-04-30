f97b1a97b0a933a34acb755658d235846bba0e37
Pingvinberg <Pingvinberg@gmail.com>
2022-04-30 22:51:33 +0200

    test

--- GhostReservation/Form1.cs
+++ GhostReservation/Form1.cs
@@ -17,12 +17,8 @@ namespace GhostReservation
         }
 
         private void btnSearch_Click_1(object sender, EventArgs e)
-        {            
-            if (SupplierArticleIDBox.Text != String.Empty && articleIDBox.Text == String.Empty)
-            {
-                SupplierArticleIDBox.Text = sqlQueryArticleID(articleIDBox.Text);
-            }
-            else if (WithErrors())
+        {
+            if (WithErrors())
             {
                 resultBox.Text = "One or several fields are empty";
             }
@@ -82,7 +78,7 @@ namespace GhostReservation
             string sql = null;            
             SqlConnection connection;               
             
-            sql = " DECLARE @StoreNo as int DECLARE @SupplierArticleId as varchar(20)  SET @StoreNo = @StoreId SET @SupplierArticleId = @ArticleId," +
+            sql = " DECLARE @StoreNo as int DECLARE @SupplierArticleId as varchar(20)  SET @StoreNo = @StoreId SET @SupplierArticleId = @ArticleId" +
                   " Select TOP 1000 CustomerOrderStatus,CustomerOrderLines.ArticleID,CustomerOrderLineStatus,ExternalOrderId,OrderedQty,ReceivedQty,rex.*FROM CustomerOrders with(nolock) INNER JOIN CustomerOrderLines with(nolock) ON CustomerOrders.CustomerOrderNo = CustomerOrderLines.CustomerOrderNo INNER JOIN AllArticles with(nolock) ON AllArticles.ArticleId = CustomerOrderLines.ArticleID FULL JOIN openquery (MYSQL, 'SELECT RECEPTBESTALLNING.salesOrderId,RECEPTBESTALLNING.id,RECEPTBESTALLNING.StoreNo,ERPARTIKEL.erpId,ERPARTIKEL.varunummer,RECEPTBESTALLNING.receptBestallningsStatus_id,RECEPTBESTALLNINGRAD.ARTIKEL_ID FROM RECEPTBESTALLNING JOIN RECEPTBESTALLNINGRAD ON RECEPTBESTALLNING.id = RECEPTBESTALLNINGRAD.bestallning_id      INNER JOIN ERPARTIKEL ON RECEPTBESTALLNINGRAD.artikel_id = ERPARTIKEL.erpId where ERPARTIKEL.varunummer = @SupplierArticleId;') rex ON rex.salesOrderId = CustomerOrders.CustomerOrderId and rex.erpId = CustomerOrderLines.ArticleId WHERE AllArticles.SupplierArticleID = @SupplierArticleId AND CustomerOrders.StoreNo = @StoreNo and rex.varunummer = @SupplierArticleId and CustomerOrders.CustomerOrderStatus <> 80 and CustomerOrderStatus <> 99 order by CustomerOrders.CustomerOrderNo DESC" +
                   " Select AllArticles.ArticleID, AllArticles.SupplierArticleID, StoreArticleInfos.ArticleNo, ReservedStockQty, InStockQty, ReservedStockInOrderQty from StoreArticleInfos with(nolock) inner join AllArticles with(nolock) on AllArticles.ArticleNo = StoreArticleInfos.ArticleNo  where StoreNo = @StoreNo and AllArticles.SupplierArticleID = @SupplierArticleId";
 
@@ -92,8 +88,8 @@ namespace GhostReservation
                 connection.Open();                
                 SqlCommand command = new SqlCommand(sql, connection);
                 command.CommandType = CommandType.Text;
-                command.Parameters.AddWithValue("@StoreId", int.Parse(storeID));
-                command.Parameters.AddWithValue("@ArticleId", int.Parse(SupplierArticleId));
+                command.Parameters.AddWithValue("@StoreId", storeID);
+                command.Parameters.AddWithValue("@ArticleId", SupplierArticleId);
                 SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
                 DataTable dataTable = new DataTable();
                 dataAdapter.Fill(dataTable);                              
